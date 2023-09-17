8d0f3543df6926e86114632610ce36937d5678ca
laercio-carvalho <laercio_ti-linux@yahoo.com>
2023-09-17 18:57:09 -0300

    Neste ultimo eu apenas fiz verificacoes se sao inteiros ou nao, finito se é um numero nao valido ou negativo

--- fundamentos-da-linguagem-dart/meu-primeiro-app-dart/meu_app/bin/meu_app.dart
+++ fundamentos-da-linguagem-dart/meu-primeiro-app-dart/meu_app/bin/meu_app.dart
@@ -32,5 +32,21 @@ void main(List<String> arguments) {
   print('Retorne verdadeiro se e somente se esse numero inteiro for Impar');
   print(numero1.isOdd);
   print(numero2.isOdd);
+  
+  print("Retorne se o numero é finito");
+  print(numero1.isFinite);
 
+  print("Retorne se o numero é infinito");
+  print(numero2.isInfinite);
+
+  print("Retorne se o numero não é valido");
+  print(numero2.isNaN);
+
+  print("Retorne se o numero é negativo");
+  print((numero2 * -1).isNegative);
+  print((numero2 * 10).isNegative);
+
+  print("Converte String para Inteiro, se não coseguir informa que é null");
+  print(int.parse("42"));
+  print(int.tryParse("teste"));
 }

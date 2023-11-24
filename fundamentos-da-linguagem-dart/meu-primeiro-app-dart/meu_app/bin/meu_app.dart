769c6c574d528192b8909a08c25b8d2f2dd59324
laercio-carvalho <laercio_ti-profissional@yahoo.com.br>
2023-11-23 23:36:32 -0300

    Neste aqui foi trabalhado com as possibilidades de Strings e Boolean.

--- fundamentos-da-linguagem-dart/meu-primeiro-app-dart/meu_app/bin/meu_app.dart
+++ fundamentos-da-linguagem-dart/meu-primeiro-app-dart/meu_app/bin/meu_app.dart
@@ -19,11 +19,17 @@ void main(List<String> arguments) {
 
   double numero4 = 10.1;
   var numero5 = 11.9;
+
+  String texto2 = "Dio";
+  var texto3 = "Trilha de Flutter e DART na DIO";
+
+  var varTrue = true;
+  bool varFalse = false;
   //Final das variaveis
 
   print('A resposta universal para tudo : ${meu_app.calculate()}!');
   print('Eu não ousaria apagar o que o grande mochileiro nos revelou.');
-  String texto3;
+  //String texto3;
   print('O valor do produto é: ${meu_app.taxa_importacao()*valor_produto*meu_app.icms()}');
   print(texto);
   print('Está aqui é o meu vetor Lista1: ''$lista1'' e Lista 2: ''$lista2');
@@ -58,6 +64,8 @@ void main(List<String> arguments) {
   print(double.parse("42"));
   print(numero3);
 
+  //Double
+
   print("Remove Ponto flutuante");
   print(numero4.truncate());
   
@@ -90,4 +98,54 @@ void main(List<String> arguments) {
   print("Converte String para Double");
   print(double.parse("10.123"));
   print(double.tryParse("Teste"));
+
+  //String
+  print(texto2);
+  print(texto3);
+
+  print("Comparacao se Vazio");
+  print(texto3.isEmpty);
+  print("".isEmpty);
+  print(" ".isEmpty);
+
+  print("Obtem o tamanho da String");
+  print(texto3.length);
+
+  print("Maiuscula / Minuscula");
+  print(texto3.toUpperCase());
+  print(texto3.toLowerCase());
+  
+  print("Se uma String contem na outra");
+  print(texto3.contains(texto2.toUpperCase()));
+
+  print("Obtem parte da String");
+  //apartir da posicao 5
+  print(texto3.substring(5));
+  //da posicao 1 até a posicao 5
+  print(texto3.substring(1,5));
+
+  print("Obtem a posicao de um texto em uma String");
+  print(texto3.indexOf("Flutter"));
+  print(texto3.indexOf("T"));
+  print(texto3.indexOf("@"));
+
+  print("Substitui uma String por outra");
+  print(texto3.replaceAll("a", "@"));
+
+  print("Quebra uma String por um caractere especifico transformando em uma ARRAY");
+  print(texto3.split(" "));
+  print("NOME;ENDERECO;CEP".split(";"));
+
+  print("Remove espaços");
+  print(" DIO ".trim());
+  print(" DIO ".trimRight());
+  print(" Texto ".trimLeft());
+
+  //Boolean
+  print("Booleano True ou False");
+  print(varTrue);
+  print(varFalse);
+  print("Esta Negacao é ${!varTrue}");
+  print("Esta negação é ${!varFalse}");
+  print(varTrue == varFalse);
 }

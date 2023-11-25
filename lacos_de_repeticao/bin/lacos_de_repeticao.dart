be1ae4b18fa7d27d17fc41eab8a8704d17f1ea94
laercio-carvalho <laercio_ti-profissional@yahoo.com.br>
2023-11-25 07:42:59 -0300

    Inicializacao para estudo de lacos e repeticoes.

---
+++ lacos_de_repeticao/bin/lacos_de_repeticao.dart
@@ -0,0 +1,5 @@
+import 'package:lacos_de_repeticao/lacos_de_repeticao.dart' as lacos_de_repeticao;
+
+void main(List<String> arguments) {
+
+}
---
+++ lacos_de_repeticao/test/lacos_de_repeticao_test.dart
@@ -0,0 +1,8 @@
+import 'package:lacos_de_repeticao/lacos_de_repeticao.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

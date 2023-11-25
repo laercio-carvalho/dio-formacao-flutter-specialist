bda5667b7f142149d47117eb65d494136175fb82
laercio-carvalho <arcano.mephisto@gmail.com>
2023-11-25 09:02:00 -0300

    Primeira iteraracao com o for. Nesse caso ele pede para dar uma entrada de dados e perguntando quantas vezes vai entrar um numero e fazer uma soma e depois podemos colocar os numeros e ele vai aparecer o resultado das somas em cada etapa.
    E no for anterior ele verifica uma quantidade de elementos em uma lista e separa-os pela quantidade de elementos.

--- lacos_de_repeticao/bin/lacos_de_repeticao.dart
+++ lacos_de_repeticao/bin/lacos_de_repeticao.dart
@@ -1,5 +1,28 @@
-import 'package:lacos_de_repeticao/lacos_de_repeticao.dart' as lacos_de_repeticao;
+//import 'package:lacos_de_repeticao/lacos_de_repeticao.dart' as lacos_de_repeticao;
+
+import 'dart:convert';
+import 'dart:io';
 
 void main(List<String> arguments) {
+  //var texto = "Meu primeiro bloco for";
+  var listaNomes = ["Laercio", "Leandro", "Larissa"];
+  var resultado = 0;
+  var quantidade = 0;
+  print("Informe a quantidade de numeros: ");
+  var line = stdin.readLineSync(encoding: utf8);
+  quantidade = int.parse(line ?? "0");
+
+  for (var i = 0; i < listaNomes.length ; i++) {
+  //print(texto[i]);
+    print(listaNomes[i]);
+
+  }
+  for (var i = 1; i <= quantidade ; i++) {
+    print("Informe o numero: ");
+    line = stdin.readLineSync(encoding: utf8);
+    var numero = int.parse(line ?? "0");
+    resultado = resultado + numero;
+    print("$i - $resultado");
+  }
 
 }
